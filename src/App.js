d512ec484369887aff3dd0b9cbcada26678ed552
Brandon Robitaille <robitaille.brandon@gmail.com>
2022-06-07 23:24:40 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  const name = 'John';
+  
+  return (
+    <div className="App">
+      <h1>Hello, {name}!</h1>  
+    </div>
+  );
+}
+
+export default App;
